80fb062903a17642ab12dff7144d1831da5c21e0
jnyou <1447177020@qq.com>
2020-06-02 22:17:19 +0800

    update login interface

--- vue-admin-online-edu/config/dev.env.js
+++ vue-admin-online-edu/config/dev.env.js
@@ -4,5 +4,6 @@ const prodEnv = require('./prod.env')
 // 修改访问后端的接口地址
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
+  // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
+  BASE_API: '"http://localhost:8001"',
 })
