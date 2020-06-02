3f78ff4160024a89ac21bb5da81e55373d4f86c3
jnyou <1447177020@qq.com>
2020-06-02 18:06:36 +0800

    init vue-admin online-edu project

---
+++ vue-admin-online-edu/config/dev.env.js
@@ -0,0 +1,8 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+// 修改访问后端的接口地址
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
+})
