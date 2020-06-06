eec66e7b03e40974f5ec99e056bd957749b35a81
jnyou <1447177020@qq.com>
2020-06-06 11:01:59 +0800

    complete teacher module uploadAvatar function and save mysql

--- vue-admin-online-edu/src/api/login.js
+++ vue-admin-online-edu/src/api/login.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function login(username, password) {
   return request({
-    url: '/edu/test/login',
+    url: '/eduservice/test/login',
     method: 'post',
     data: {
       username,
@@ -13,7 +13,7 @@ export function login(username, password) {
 
 export function getInfo(token) {
   return request({
-    url: '/edu/test/info',
+    url: '/eduservice/test/info',
     method: 'get',
     params: { token }
   })
--- vue-admin-online-edu/src/views/edu/teacher/save.vue
+++ vue-admin-online-edu/src/views/edu/teacher/save.vue
@@ -25,10 +25,37 @@
       </el-form-item>
 
       <!-- 讲师头像：TODO -->
+      <!-- 讲师头像 -->
+      <el-form-item label="讲师头像">
 
-      <el-form-item>
-        <el-button :disabled="saveBtnDisabled" type="primary" @click="saveOrUpdate">保存</el-button>
-      </el-form-item>
+        <!-- 头衔缩略图 -->
+        <pan-thumb :image="teacher.avatar"/>
+        <!-- 文件上传按钮 -->
+        <el-button type="primary" icon="el-icon-upload" @click="imagecropperShow=true">更换头像
+        </el-button>
+
+        <!--
+          v-show：是否显示上传组件
+          :key：类似于id，如果一个页面多个图片上传控件，可以做区分
+          :url：后台上传的url地址
+          @close：关闭上传组件
+          field : 对应后端MultipartFile mpf参数名称mpf
+          @crop-upload-success：上传成功后的回调 -->
+            <image-cropper
+                          v-show="imagecropperShow"
+                          :width="300"
+                          :height="300"
+                          :key="imagecropperKey"
+                          :url="BASE_API+'/eduoss/fileoss/uploadOssFile'"
+                          field="mpf"  
+                          @close="close"
+                          @crop-upload-success="cropSuccess"/>
+
+        </el-form-item>
+
+        <el-form-item>
+          <el-button :disabled="saveBtnDisabled" type="primary" @click="saveOrUpdate">保存</el-button>
+        </el-form-item>
     </el-form>
   </div>
 </template>
@@ -36,14 +63,20 @@
 <script>
 // 引入teacher.js
 import teacherApi from '@/api/edu/teacher'
+// 引入上传头像的组件
+import ImageCropper from '@/components/ImageCropper'
+import PanThumb from '@/components/PanThumb'
 
 export default {
-
+    components: { ImageCropper, PanThumb }, // 声明组件
     // 编写核心代码
     data(){ // 定义变量和初始值
         return {
             saveBtnDisabled: false, // 保存按钮是否禁用，防止重复提交
-            teacher: {} // 参数teacher对象
+            teacher: {}, // 参数teacher对象
+            imagecropperShow: false, // 是否显示上传组件
+            imagecropperKey: 0, // 上传组件id
+            BASE_API:process.env.BASE_API, // 获取dev.env.js中接口API地址
         }
         
     },
@@ -67,14 +100,15 @@ export default {
             this.getTeacherInfoById(id)
           } else{
             // 点击添加讲师清空表单，多次路由跳转到同一页面,只会执行一次，后面再进行跳转不会执行，使用路由监听
-            this.teacher = {}
+            this.teacher = {
+              avatar: "https://jnyou.oss-cn-beijing.aliyuncs.com/u%3D2529138109%2C771074326%26fm%3D26%26gp%3D0.jpg"
+            }
           }
         },
         // 保存方法
         saveOrUpdate(){
           // 根据teacher对象是否有id进行判断保存还是修改
           if(!this.teacher.id){
-            this.teacher = {}
             // 添加
             this.saveTeacher()
           }else{
@@ -113,6 +147,17 @@ export default {
             // 回到列表页面 路由跳转
             this.$router.push({ path: '/teacher/list' })
           })
+        },
+        close(){ // 关闭上传头像的弹框
+          this.imagecropperShow = false
+          this.imagecropperKey = this.imagecropperKey + 1
+        },
+        cropSuccess(data){  //上传成功的回调
+          // 上传成功的回调
+          this.imagecropperShow = false
+          // 获取接口返回的url地址
+          this.teacher.avatar = data.url
+          this.imagecropperKey = this.imagecropperKey + 1
         }
     }
     
