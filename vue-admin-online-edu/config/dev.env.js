e2c50e96aa2c4bea1194140ba53ce463c60b5fac
jnyou <1447177020@qq.com>
2020-06-07 12:31:08 +0800

    complete subject category module and test

--- vue-admin-online-edu/config/dev.env.js
+++ vue-admin-online-edu/config/dev.env.js
@@ -6,4 +6,5 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
   BASE_API: '"http://localhost:9001"',
+  OSS_PATH: '"https://jnyou.oss-cn-beijing.aliyuncs.com"', // OSS服务地址
 })
