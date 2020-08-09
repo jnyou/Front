71cded114ac6f9de7df40de015d0e8ed90de7cf8
jnyou <1447177020@qq.com>
2020-08-09 15:48:49 +0800

    实现课程评论和查看功能

--- vue-front-online-edu/pages/course/_id.vue
+++ vue-front-online-edu/pages/course/_id.vue
@@ -155,12 +155,96 @@
         <div class="clear"></div>
       </div>
     </section>
-    <!-- /课程详情 结束 -->
+    <div class="mt50 commentHtml"><div>
+      <h6 class="c-c-content c-infor-title" id="i-art-comment">
+        <span class="commentTitle">课程评论</span>
+      </h6>
+      <section class="lh-bj-list pr mt20 replyhtml">
+        <ul>
+          <li class="unBr">
+            <aside class="noter-pic">
+              <img width="50" height="50" class="picImg" src="~/assets/img/avatar-boy.gif">
+              </aside>
+            <div class="of">
+              <section class="n-reply-wrap">
+                <fieldset>
+                  <textarea name="" v-model="comment.content" placeholder="输入您要评论的文字" id="commentContent"></textarea>
+                </fieldset>
+                <p class="of mt5 tar pl10 pr10">
+                  <span class="fl "><tt class="c-red commentContentmeg" style="display: none;"></tt></span>
+                  <input type="button" @click="addComment()" value="回复" class="lh-reply-btn">
+                </p>
+              </section>
+            </div>
+          </li>
+        </ul>
+      </section>
+      <section class="">
+          <section class="question-list lh-bj-list pr">
+            <ul class="pr10">
+              <li v-for="(comment,index) in data.items" v-bind:key="index">
+                  <aside class="noter-pic">
+                    <img width="50" height="50" class="picImg" :src="comment.avatar">
+                    </aside>
+                  <div class="of">
+                    <span class="fl"> 
+                    <font class="fsize12 c-blue"> 
+                      {{comment.nickname}}</font>
+                    <font class="fsize12 c-999 ml5">评论：</font></span>
+                  </div>
+                  <div class="noter-txt mt5">
+                    <p>{{comment.content}}</p>
+                  </div>
+                  <div class="of mt5">
+                    <span class="fr"><font class="fsize12 c-999 ml5">{{comment.gmtCreate}}</font></span>
+                  </div>
+                </li>
+              
+              </ul>
+          </section>
+        </section>
+        
+        <!-- 公共分页 开始 -->
+        <div class="paging">
+            <!-- undisable这个class是否存在，取决于数据属性hasPrevious -->
+            <a
+            :class="{undisable: !data.hasPrevious}"
+            href="#"
+            title="首页"
+            @click.prevent="gotoPage(1)">首</a>
+            <a
+            :class="{undisable: !data.hasPrevious}"
+            href="#"
+            title="前一页"
+            @click.prevent="gotoPage(data.current-1)">&lt;</a>
+            <a
+            v-for="page in data.pages"
+            :key="page"
+            :class="{current: data.current == page, undisable: data.current == page}"
+            :title="'第'+page+'页'"
+            href="#"
+            @click.prevent="gotoPage(page)">{{ page }}</a>
+            <a
+            :class="{undisable: !data.hasNext}"
+            href="#"
+            title="后一页"
+            @click.prevent="gotoPage(data.current+1)">&gt;</a>
+            <a
+            :class="{undisable: !data.hasNext}"
+            href="#"
+            title="末页"
+            @click.prevent="gotoPage(data.pages)">末</a>
+            <div class="clear"/>
+        </div>
+        <!-- 公共分页 结束 -->
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import courseApi from '@/api/course'
+import comment from '@/api/commonedu'
 export default {
 
   asyncData({ params, error }) {
@@ -168,9 +252,68 @@ export default {
       console.log(response);
       return { 
         course: response.data.data.course,
-        chapterList: response.data.data.chapterVoList
+        chapterList: response.data.data.chapterVoList,
+        courseId: params.id
       }
     })
+  },
+  data() {
+    return {
+      data:{},
+      page:1,
+      limit:4,
+      total:10,
+      comment:{
+        content:'',
+        courseId:this.courseId
+      },
+      courseInfo:{},
+      chapterVideoList:[],
+      isbuyCourse:false
+    }
+  },
+  created() {
+    this.initCourseInfo()
+    this.initComment()
+  },
+  methods:{
+    //获取课程详情
+    initCourseInfo() {
+      courseApi.getById(this.courseId)
+            .then(response => {
+              this.courseInfo=response.data.data.course
+              this.chapterVideoList=response.data.data.chapterVoList
+              this.isbuyCourse=response.data.data.isbuyCourse
+            })
+    },
+
+    initComment(){
+       comment.getPageList(this.page, this.limit, this.courseId).then(response => {
+           this.data = response.data.data
+       })
+    },
+    addComment(){
+        this.comment.courseId = this.courseId
+        this.comment.teacherId = this.courseInfo.teacherId
+        comment.addComment(this.comment).then(response => {
+          if (response.data.code == 28004) {
+              console.log("response.data.resultCode是28004")
+              // 返回 错误代码-1 清除ticket信息并跳转到登录页面
+              //debugger
+              window.location.href="/login"
+              return
+          }
+          if(response.data.success){
+                this.comment.content = ''
+                this.initComment()
+          }  
+        })
+    },
+    gotoPage(page){
+          comment.getPageList(page, this.limit,this.courseId).then(response => {
+              this.data = response.data.data
+          })
+      }
   }
 };
 </script>
