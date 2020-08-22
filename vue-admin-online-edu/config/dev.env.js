3d7f1110de32a5cd7d58614c120c92a93c0ca6c9
jnyou <1447177020@qq.com>
2020-08-22 23:03:21 +0800

    initial gateway in project

--- vue-admin-online-edu/config/dev.env.js
+++ vue-admin-online-edu/config/dev.env.js
@@ -5,6 +5,7 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
-  BASE_API: '"http://localhost:9001"',
+  // BASE_API: '"http://localhost:9001"', // nginx端口
+  BASE_API: '"http://localhost:8222"', // gateway端口
   OSS_PATH: '"https://jnyou.oss-cn-beijing.aliyuncs.com"', // OSS服务地址
 })
